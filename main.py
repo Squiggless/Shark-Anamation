0aeb827cefed95178f30f540e7b4e9ec7f5be181
Squiggless <quinnwaldron@icloud.com>
2020-08-10 18:57:12 +0000

    Made it a cooler immage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+
 import pygame
 
 from pygame.locals import *
@@ -8,14 +9,19 @@ pygame.init()
 screen_info = pygame.display.Info()
 size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
 screen = pygame.display.set_mode(size)
-color = (5, 205, 250)
+color = (13, 41, 133)
 
 
 clock = pygame.time.Clock()
+
+sprite_image = pygame.image.load("Shark.png")
+sprite_rect = sprite_image.get_rect()
+
 def main():
 	while True:
 		clock.tick(60)
 		screen.fill(color)
+		screen.blit(sprite_image, sprite_rect)
 		pygame.display.flip()
 
 main()
