1a4115c8f05f6bff4711b21941a21e2aa3597fad
Squiggless <quinnwaldron@icloud.com>
2020-08-10 19:07:06 +0000

    made sprith the right size

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ pygame.init()
 screen_info = pygame.display.Info()
 size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
 screen = pygame.display.set_mode(size)
-color = (13, 41, 133)
+color = (13, 36, 115)
 
 
 clock = pygame.time.Clock()
 
-sprite_image = pygame.image.load("Shark.png")
+sprite_image = pygame.image.load("dimir praetor.jpg")
 sprite_rect = sprite_image.get_rect()
+sprite_image = pygame.transform.scale(sprite_image,(int(0.3*sprite_rect.width), int(0.3*sprite_rect.height)))
+sprite_rect = sprite_image.get_rect()
+sprite_rect.center = width // 2, height // 2
+
 
 def main():
 	while True:
