53b03bc4278f015e109a496280c8111858f328f4
Squiggless <quinnwaldron@icloud.com>
2020-08-10 18:38:16 +0000

    ?

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from pygame.locals import *
 
 pygame.init()
 
-screen_info = pygame.display.info()
-size = (width, height) = (int(screen_info.current_w), int(screen_ info.current_h))
+screen_info = pygame.display.Info()
+size = (width, height) = (int(screen_info.current_w), int(screen_info.current_h))
 screen = pygame.display.set_mode(size)
+color = (7, 49, 117)
+
+
+clock = pygame.time.Clock()
+def main():
+	while True:
+		clock.tick(60)
+		screen.fill(color)
+		pygame.display.flip()
+
+main()
